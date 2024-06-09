3fdfbb727245849e4da4ab5ef9a84d8da9e67cfe
dung204 <acezombiev4@gmail.com>
2024-06-09 14:09:48 +0700

    feat: footer

---
+++ src/layouts/Footer/Footer.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const FooterStyle = styled.div`
+  background-color: rgb(32, 32, 32);
+  color: white;
+`;
+
+export default FooterStyle;
--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,7 +1,23 @@
+import { Col, Row } from 'react-bootstrap';
+
+import FooterStyle from '@/layouts/Footer/Footer.style';
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <FooterStyle>
+      <footer className="w-100">
+        <Row className="justify-content-center">
+          <Col xs={6} className="text-center py-4">
+            <p>&copy; 2024 Rave Haven. All right reserved.</p>
+            <p>
+              Made by{' '}
+              <a target="_blank" href="https://github.com/dung204">
+                Mantrilogix - dung204 - Ho Anh Dung
+              </a>
+            </p>
+          </Col>
+        </Row>
+      </footer>
+    </FooterStyle>
   );
 }
